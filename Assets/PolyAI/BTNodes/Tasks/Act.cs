4b73efc425a3e9aff7e274b16dcf7a0b51aaacf7
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:12:16 +0100

    Update Act.cs
    Done

--- Assets/PolyAI/BTNodes/Tasks/Act.cs
+++ Assets/PolyAI/BTNodes/Tasks/Act.cs
@@ -2,13 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Act : BTNode
+public class Act : Task
 {
-    Animation action;
+    private string action;
+    private bool active;
+
+    /// <summary>
+    /// Will call an animator's bool parameter.
+    /// </summary>
+    /// <param name="_agent">Agent's reference.</param>
+    /// <param name="_action">Parameter's name.</param>
+    /// <param name="_value">Parameter's value.</param>
+    public Act(PolyAgentBase _agent, string _action) : base(_agent)
+    {
+        action = _action;
+    }
 
     public override NodeState Tick()
-    {
-        Debug.Log("Acting");
-        return NodeState.Success;
+    {   
+        Debug.Log(action);
+        Animator animator = agent.GetComponent<Animator>();
+        if (animator)
+        {
+            if (animator.GetBool(action))
+            {
+                // Get most recent anim
+                AnimatorStateInfo animState = animator.GetNextAnimatorStateInfo(0);
+                if (animState.IsName(""))
+                {
+                    animState = animator.GetCurrentAnimatorStateInfo(0);
+                }
+                // Is anim finished
+                if (animState.normalizedTime >= 1.0f)
+                {
+                    animator.SetBool(action, false);
+                    state = NodeState.Success;
+                }
+            }
+            else
+            {
+                animator.SetBool(action, true);
+                state = NodeState.Running;
+            }
+        }
+        else
+        {
+            Debug.LogError("There are no animator on the agent.");
+            state = NodeState.Error;
+        }
+        return state;
     }
 }

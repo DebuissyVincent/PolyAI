fc340bf1d75cff7c90cb229d8f015dcccd4e634a
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:11:38 +0100

    Update ForceStatus.cs
    Done

--- Assets/PolyAI/BTNodes/Decorators/Misc/ForceStatus.cs
+++ Assets/PolyAI/BTNodes/Decorators/Misc/ForceStatus.cs
@@ -6,6 +6,12 @@ public class ForceStatus : Decorator
 {
     NodeState forcedState;
 
+    /// <summary>
+    /// Non-blocking decorator. Will return a fixed state no matter the child's state.
+    /// WARNING : May cause unwanted behaviours as subsequent nodes could be forcefully interrupted.
+    /// </summary>
+    /// <param name="_forcedState">The state which will be forcefully returned.</param>
+    /// <param name="_isNot">Will have no effect.</param>
     public ForceStatus(NodeState _forcedState, bool _isNot = false) : base(_isNot)
     {
         forcedState = _forcedState;
@@ -14,7 +20,6 @@ public class ForceStatus : Decorator
     public override NodeState Tick()
     {
         state = child.Tick();
-
         return forcedState;
     }
 }

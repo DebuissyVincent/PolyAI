409fd0348c81319978c3401ed048d27f94b02d71
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:11:11 +0100

    Update ConditionalLoop.cs
    Should be done

--- Assets/PolyAI/BTNodes/Decorators/Logic/ConditionalLoop.cs
+++ Assets/PolyAI/BTNodes/Decorators/Logic/ConditionalLoop.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConditionnalLoop : Decorator
+public class ConditionalLoop : Decorator
 {
-    Decorator condtion;
+    Decorator condition;
+    bool untilChildIsDone;
+
+    public ConditionalLoop(Decorator _condition, bool _untilChildIsDone = false, bool _isNot = false) : base(_isNot)
+    {
+        condition = _condition;
+        untilChildIsDone = _untilChildIsDone;
+    }
 
     public override NodeState Tick()
     {
-        return NodeState.Failure;
+        state = condition.Tick();
+        if (state != NodeState.Failure)
+        {
+            state = child.Tick();
+            if (!untilChildIsDone)
+            {
+                return NodeState.Running;
+            }
+        }
+        return state;
     }
 }

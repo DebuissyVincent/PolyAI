c7e8e8d591cc4108378385e196d047b4e90ddb40
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:15:08 +0100

    Update Wait.cs
    Done

--- Assets/PolyAI/BTNodes/Tasks/Wait.cs
+++ Assets/PolyAI/BTNodes/Tasks/Wait.cs
@@ -2,37 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wait : ConstantUpdateNode
+public class Wait : BTNode, ConstantUpdateNode
 {
     private float duration;
     private float timer;
+    //private bool forceTickUponEnd;
 
     public Wait(float _duration = 1.0f)
     {
         duration = _duration;
+        //forceTickUponEnd = _forceTickUponEnd;
         PolyAIManager.Instance.ConstantUpdateNodes.Add(this);
     }
 
-    public override void Update()
+    public void Update()
     {
-        timer -= Time.deltaTime;
+        if (timer > 0.0f)
+        {
+            timer -= Time.deltaTime;
+        }
+        //else if (forceTickUponEnd)
+        //{
+        //    return true;
+        //}
+        //return false;
     }
 
     public override NodeState Tick()
     {
         if (state != NodeState.Running)
         {
-            Debug.Log("Waiting");
             timer = duration;
-            needsUpdate = true;
             state = NodeState.Running;
         }
-        // When duration is 0.0f, waits for one frame.
+        // When duration is 0.0f, waits for one-two frame.
         else
         {
             if (timer <= 0.0f)
             {
-                needsUpdate = false;
                 state = NodeState.Success;
             }
         }

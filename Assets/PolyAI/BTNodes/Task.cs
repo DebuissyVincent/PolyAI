60f9128e03111e4c37223049845d445f5f63c12e
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:12:02 +0100

    Update Task.cs
    Linked tasks to agent

--- Assets/PolyAI/BTNodes/Task.cs
+++ Assets/PolyAI/BTNodes/Task.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Task
+public abstract class Task : BTNode
 {
+    protected PolyAgentBase agent;
 
+    protected Task(PolyAgentBase _agent)
+    {
+        agent = _agent;
+    }
 }

040fbd617fe9702362dc487ba644544e606a2a52
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:05:47 +0100

    Added KnowledgeDecorator
    Linked Decorators to KnowledgeBase

---
+++ Assets/PolyAI/BTNodes/Decorators/Knowledge/KnowledgeDecorator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class KnowledgeDecorator : Decorator
+{
+    protected KnowledgeBase agentKnowledge;
+
+    protected KnowledgeDecorator(KnowledgeBase _agentKnowledge, bool _isNot) : base(_isNot)
+    {
+        agentKnowledge = _agentKnowledge;
+    }
+}

3cee94d73aade91ca84dfbd43b32cb6b0a377eb0
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:13:27 +0100

    Create ActSetup.cs
    Linked Act task to unity inspector

---
+++ Assets/PolyAI/BTNodes/Tasks/Setup/ActSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActSetup : TaskSetup
+{
+    [SerializeField] private string action;
+
+    public override Task SetupTask()
+    {
+        Task task = new Act(GetComponent<PolyAgentBase>(), action);
+        return task;
+    }
+}

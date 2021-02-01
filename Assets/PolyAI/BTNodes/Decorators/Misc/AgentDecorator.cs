ad927e2f6f30cd964be53e9fe3d0081d39981dbb
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:10:11 +0100

    Added Agent Decorator
    Linked decorators to agents

---
+++ Assets/PolyAI/BTNodes/Decorators/Misc/AgentDecorator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AgentDecorator : Decorator
+{
+    protected PolyAgentBase agent;
+
+    public AgentDecorator(PolyAgentBase _agent, bool _isNot) : base (_isNot)
+    {
+        agent = _agent;
+    }
+}

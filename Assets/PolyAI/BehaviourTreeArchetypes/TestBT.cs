9d1a6098120cd2628c7d38ad0776571a9b602370
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:16:56 +0100

    Update TestBT.cs
    First functional Behaviour Tree done

--- Assets/PolyAI/BehaviourTreeArchetypes/TestBT.cs
+++ Assets/PolyAI/BehaviourTreeArchetypes/TestBT.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class TestBT : BTSetup
 {
-    public override void BuildTree(BehaviourTree _bt, KnowledgeBase _privateKlg)
+    public override void BuildTree(BehaviourTree _bt, PolyAgentBT _agent)
     {
-        Sequence sequence = new Sequence();
-
+        Sequence sequence = new Sequence(true); // By default, the sequence won't break upon the failure of his children
         _bt.SetFirstNode(sequence);
 
-        sequence.Child.Add(new MoveTo());
+        // Begin sequence
+        IsAtPosition isAtPos = new IsAtPosition(_agent); // Check for nav agent destination
+        isAtPos.SetChild(new MoveTo(_agent, Vector3.zero));
+        sequence.Child.Add(isAtPos);
+
         sequence.Child.Add(new Wait(0.5f));
-        sequence.Child.Add(new Act());
+        sequence.Child.Add(new Act(_agent, "Acting"));
         sequence.Child.Add(new Wait()); // By default, waits for 1 second
 
         Selector selector = new Selector();
-
         sequence.Child.Add(selector);
 
-        ForceStatus forceStatus = new ForceStatus(NodeState.Failure);
+        // Begin Selector
+        Sequence sequence2 = new Sequence();
+        selector.Child.Add(sequence2);
 
-        selector.Child.Add(forceStatus);
+        // Begin sequence 2
+        IsAtPosition isAtPos2 = new IsAtPosition(_agent, Vector3.zero);
+        isAtPos2.SetChild(new Act(_agent, "Acting"));
+        sequence2.Child.Add(isAtPos2);
 
-        forceStatus.SetChild(new MoveTo());
+        sequence2.Child.Add(new MoveTo(_agent, Vector3.forward * 3.0f));
+        // End sequence 2
 
-        selector.Child.Add(new Act());
         selector.Child.Add(new Wait(5.0f));
+        // End selector
+        // End sequence
     }
 }

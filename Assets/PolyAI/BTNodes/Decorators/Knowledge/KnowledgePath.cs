a276d5629905f8e6af762a4cf7a86b7e8a86addd
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:10:52 +0100

    Update KnowledgePath.cs
    Started setuping node

--- Assets/PolyAI/BTNodes/Decorators/Knowledge/KnowledgePath.cs
+++ Assets/PolyAI/BTNodes/Decorators/Knowledge/KnowledgePath.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnowledgePath : Decorator
+public class KnowledgePath : KnowledgeDecorator
 {
-    string knowlegePos1;
+    string knowlegePos;
     string knowledgePos2;
+
+    public KnowledgePath(Knowledge _klg, string _klgPos, string _klgPos2, bool _isNot = false) : base(_klg, _isNot)
+    {
+        knowlegePos = _klgPos;
+        knowledgePos2 = _klgPos2;
+    }
 
     public override NodeState Tick()
     {
-        return NodeState.Failure;
+        return state;
     }
 }

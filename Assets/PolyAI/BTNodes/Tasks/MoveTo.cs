beda24373d19a1d56fb5ed6a1168f876e87ec5ff
Vincent Debuissy <vincent.debuissy.pro@gmail.com>
2021-02-01 21:12:57 +0100

    Update MoveTo.cs
    Done

--- Assets/PolyAI/BTNodes/Tasks/MoveTo.cs
+++ Assets/PolyAI/BTNodes/Tasks/MoveTo.cs
@@ -1,31 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class MoveTo : BTNode
+public class MoveTo : Task
 {
     private Vector3 destination;
     private float minRange;
     private float maxRange;
 
+    /// If _maxRange < 0.0f, will keep default nav mesh agent stopping distance.
+    public MoveTo(PolyAgentBase _agent, Vector3 _destination, float _minRange = 0.0f, float _maxRange = -1.0f) : base(_agent)
+    {
+        destination = _destination;
+        maxRange = _maxRange;
+        minRange = _minRange;
+    }
+
+    public MoveTo(PolyAgentBase _agent, string _destinationKlg, float _minRange = 0.0f, float _maxRange = -1.0f) : base(_agent)
+    {
+        Knowledge klg = (Knowledge)agent.PrivateKlg;
+        if (klg != null)
+        {
+            Vector3 dest = (Vector3)klg.GetKnowledge(_destinationKlg);
+            if (dest != null)
+            {
+                destination = dest;
+            }
+            else
+            {
+                Debug.LogError("Retrieved custom knowledge has wrong type.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Tried using custom knowledge with KnowledgeBase.");
+        } 
+        maxRange = _maxRange;
+        minRange = _minRange;
+    }
+
     public override NodeState Tick()
     {
-        Debug.Log("Moving to : " + destination.ToString());
+        NavMeshAgent nav = agent.GetComponent<NavMeshAgent>();
+        // Path is not available or must be updated
+        if ((!nav.hasPath && !nav.pathPending) || nav.destination != destination)
+        {
+            // Path can be done
+            if (nav.SetDestination(destination))
+            {
+                if (maxRange >= 0.0f)
+                {
+                    nav.stoppingDistance = maxRange;
+                }
+                state = NodeState.Running;
+                Debug.Log("Moving to " + destination.ToString());
+            }
+            // Path cannot be done
+            else
+            {
+                Debug.Log("Can't move to " + destination.ToString());
+                state = NodeState.Failure;
+            }
+        }
+        // Path already set
+        else
+        {
+            // Agent is closest to destination as can be
+            if (nav.remainingDistance <= minRange)
+            {
+                nav.isStopped = true;
+                state = NodeState.Success;
+                Debug.Log("Moving stoped. Agent as close as need be.");
+            }
+            // Agent is at destination
+            else if (nav.remainingDistance <= nav.stoppingDistance)
+            {
+                if (!nav.hasPath || nav.velocity.sqrMagnitude == 0f)
+                {
+                    state = NodeState.Success;
+                    Debug.Log("Moving stoped. Agent is close enough.");
+                }
+            }
+        }
         return NodeState.Success;
-    }
-
-    public void SetDestination(Vector3 _dest)
-    {
-        destination = _dest;
-    }
-
-    public void SetMinRange(float _range)
-    {
-        minRange = _range;
-    }
-
-    public void SetMaxRange(float _range)
-    {
-        maxRange = _range;
     }
 }
